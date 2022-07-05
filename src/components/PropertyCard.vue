de3a444524b25b861f45be8a2d4783c754a7f8e7
Joshua Steed <joshuasteed123@gmail.com>
2022-07-05 16:28:25 +0200

    new commmit

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -42,4 +42,129 @@ export default {
   props: ["property"],
 };
 </script>
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Roboto:wght@100;300;400;500;700;900&display=swap");
+
+.box {
+  height: max-content;
+  width: max-content;
+  background: transparent;
+  flex-basis: 33.333333%;
+  border-radius: 4px 4px;
+  transition: all 0.7s ease-in-out;
+}
+
+#box-container {
+  padding-left: 0;
+  display: flex;
+  flex-wrap: wrap;
+  height: fit-content;
+  row-gap: 20px;
+  column-gap: 20px;
+  justify-content: center;
+  padding-bottom: 10%;
+  /* flex-basis: 33.333333%; */
+}
+
+.house-image:hover {
+  filter: brightness(110%);
+
+  /* transform: translateY(-3px); */
+}
+.box:hover {
+  transform: translateY(-5px);
+}
+
+figure {
+  position: relative;
+}
+figcaption {
+  font-size: 20px;
+  position: absolute;
+  width: fit-content;
+  text-align: center;
+  left: 15px;
+  top: 260px;
+  color: white;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+}
+
+.house-image {
+  height: 300px;
+  /* box-shadow: 0px 0px 40px 0px rgb(0 0 0 / 30%); */
+  width: 400px;
+  border-radius: 4px 4px 0px 0px;
+  filter: brightness(70%);
+  transition: all 1s ease-in-out;
+}
+
+#discover {
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(238, 238, 238, 1) 100%
+  );
+}
+
+input[type="email"]::placeholder {
+  padding: 10px !important;
+}
+
+#title {
+  margin-top: 2%;
+  font-family: "Open Sans", sans-serif;
+  font-size: larger;
+  margin: 0 auto;
+}
+#address {
+  padding-top: 2px;
+  font-family: "Roboto", sans-serif;
+  margin: 0 auto;
+  font-size: 14px;
+}
+#bedrooms {
+  font-family: "Roboto", sans-serif;
+}
+#bathrooms {
+  font-family: "Roboto", sans-serif;
+}
+#garages {
+  font-family: "Roboto", sans-serif;
+}
+#size {
+  font-family: "Roboto", sans-serif;
+}
+#property-type {
+  font-family: "Open Sans", sans-serif;
+  font-size: 13px;
+  font-weight: 600;
+}
+
+.icons {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  column-gap: 16px;
+  font-weight: 300;
+  font-size: 13px;
+  padding-top: 10px;
+  padding-bottom: 3px;
+}
+
+#text {
+  margin-left: 1%;
+}
+
+#heading-2 {
+  padding-top: 7%;
+  font-smooth: 20em;
+  font-size: 40px;
+  font-weight: 300;
+  font-family: "Open Sans", sans-serif;
+}
+#subheading-2 {
+  padding-bottom: 5%;
+}
+</style>
